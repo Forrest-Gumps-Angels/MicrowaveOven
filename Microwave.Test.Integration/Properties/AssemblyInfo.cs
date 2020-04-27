b4a59499775d3fe6ce0b76b3016964cf38e78a4c
Morten Lenschow Hansen <au616040@uni.au.dk>
2020-04-27 12:53:35 +0200

    Added Microwave.Test.Integration, nuget package NUnit, also testing webhook

---
+++ Microwave.Test.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microwave.Test.Integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microwave.Test.Integration")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5623152d-d69b-4b4d-99c8-a42533c6bcab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Microwave.Test.Integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
